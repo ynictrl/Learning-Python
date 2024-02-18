2e68f398dd359293862cf1d6ed5a81f64109dcfb
ynictrl <ynictrl@gmail.com>
2024-02-18 12:23:45 -0300

    Strings

---
+++ CursoPythonAulas/aula027.py
@@ -0,0 +1,11 @@
+"""
+fatiamento de strings
+012345678
+Olá mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs.: a função len retorna a qtd de caracteres da srt
+"""
+variavel = 'Olá mundo'
+print(variavel[-1:-10:-1])
+print(variavel[::-1])
