fdc379cd9d74886787aa5b0b98decf415178d007
ynictrl <ynictrl@gmail.com>
2024-04-22 21:37:30 -0300

    conversão

--- Metodos_numericos/Lista_1/exec_1.py
+++ Metodos_numericos/Lista_1/exec_1.py
@@ -1,31 +1,32 @@
-num = '1110.101' 
-numPart = num.split(".")
+
+def conversion(numBin):
     
-def conversion(lista):
-    
+    numBin = numBin.split(".") # dividir em as partes 
+
     # CASA INTEIRA
-    i = len(lista[0]) - 1  # iteravel de trás pra frente
+    i = len(numBin[0]) - 1  # iteravel de trás pra frente
     int_exp = -1 # expoente
     int_res = 0 # resultado final
 
     while i >= 0:
         int_exp += 1 # acréscimo do expoente
-        if lista[0][i] != '0': int_res += 2**(int_exp) # resltado recebe apenas para '1'
+        if numBin[0][i] != '0': int_res += 2**(int_exp) # resltado recebe apenas para '1'
         i -= 1 # decremento
 
     # ---------------------------------------------------------
 
     # CASA DECIMAL
-    j = -(len(lista[1])) # iteravel de trás pra frente
+    j = -(len(numBin[1])) # iteravel de trás pra frente
     float_exp = 0 # expoente
     float_res = 0 # resultado final 
 
     while j < 0:
         float_exp -= 1 # decréscimo do expoente
-        if lista[1][j] != '0': float_res += 2**(float_exp) # resltado recebe apenas para '1'
+        if numBin[1][j] != '0': float_res += 2**(float_exp) # resltado recebe apenas para '1'
         j += 1 # acréscimo
 
     res = int_res + float_res # soma dos resultados
     return res
 
-print(conversion(numPart))
+print(conversion('11.11110101'))
+print(conversion('10110101.1010101'))
