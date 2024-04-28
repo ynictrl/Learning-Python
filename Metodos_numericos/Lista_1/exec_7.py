602198bb932867302ec88fc2903c74250dce0d0d
ynictrl <ynictrl@gmail.com>
2024-04-27 22:44:06 -0300

    exec_7

---
+++ Metodos_numericos/Lista_1/exec_7.py
@@ -0,0 +1,40 @@
+# Erro absoluto = |valor verdadeiro - valor calculado|
+# Erro relativo = |valor verdadeiro - valor calculado|/|valor verdadeiro|
+# Erro percentual = Erro absoluto * Erro relativo * 100
+
+a = [1.000, 0.999]
+b = [0.34678, 0.3468]
+c = [0.000005, 0.00000495]
+d = [15000.85, 14999.0]
+
+def calc_menor_erro(v_v, v_c): # valor ver e calc
+    erro_ver = v_v - v_c
+    if erro_ver < 0: erro_ver *= -1
+    erro_rel = erro_ver/v_v
+    if erro_rel < 0: erro_rel *= -1 
+    erro_per = erro_ver * erro_rel * 100
+
+    return erro_per
+
+
+def compara_erro_per(*args):
+
+    erros_per = []
+    for i in args:
+        erros_per.append(calc_menor_erro(i[0], i[1]))
+
+
+    menor = 0
+    for j in range(len(erros_per)):
+        if j > 0:
+            if erros_per[j] < erros_per[j-1]:
+                menor = erros_per[j]
+        else:
+            menor = j
+    
+    frase = f'valor exato: {args[j][0]} | valor exato: {args[j][1]} | erro percentual: {min(erros_per)}'
+
+    return frase
+
+print(compara_erro_per(a, b, c, d))
+                        
