fb5f7b6708cd84ea2b45985e684abf33bbcc6f45
ynictrl <ynictrl@gmail.com>
2024-09-19 20:25:47 -0300

    embaralhar

--- Programming_exercises/embaralhar.py
+++ Programming_exercises/embaralhar.py
@@ -7,20 +7,86 @@ import random
 # print(random.sample(numeros, k=14))
 # random.shuffle(numeros)
 
-l1 = [1, 2, 3, 4, 5, 6, 7]
-l2 = []
-
-for i in range(len(l1)):
-    
-    if i == 0:
-        k = random.randrange(0, len(l1))
-        lost = str(k)
-    else:
+    # public void Randomizer()
+    # {
+    #     string[] cards2 = new string[cards.Length];
+    #     int k = 0; 
+    #     //string lost = "";
         
-        while str(k) in lost:
-            k = random.randrange(0, len(l1))
-        lost += str(k)
-       
-    l2.append(l1[k])
+    #     for(int i = 0; i < cards.Length; i++)
+    #     {
+    #         if(i==0)
+    #         {
+    #             k = Random.Range(0, cards.Length);
+    #             lost = k.ToString();
+    #         }else{
 
-print(f'Lista original:    {l1} \nLista embaralhada: {l2}')
+    #             bool checkIn = true;
+
+    #             while(checkIn)
+    #             {
+    #                 for(int j=0; j<lost.Length; j++)
+    #                 {
+    #                     k = Random.Range(0, cards.Length);
+    #                     if(lost[j].ToString() == k.ToString())
+    #                     {
+    #                         checkIn = false;
+    #                     }
+    #                 }
+    #             }
+    #             lost += k.ToString();
+    #         }
+
+            
+    #         cards2[i] = cards[k];
+    #     }
+    #     cards = cards2;
+
+    # }
+
+def ComPy():
+    l1 = [1, 2, 3, 4, 5, 6, 7]
+    l2 = []
+
+    for i in range(len(l1)):
+        
+        if i == 0:
+            k = random.randrange(0, len(l1))
+            lost = str(k)
+        else:
+            
+            while str(k) in lost:
+                k = random.randrange(0, len(l1))
+            lost += str(k)
+        
+        l2.append(l1[k])
+
+    print(f'Lista original:    {l1} \nLista embaralhada: {l2}')
+
+def In(a, b):
+    res = False
+    for i in range(len(b)):
+        if a == b[i]:
+            res = True
+
+    return res
+
+def SemPy():
+    l1 = [1, 2, 3, 4, 5, 6, 7]
+    l2 = []
+    lost = ""
+
+    for i in range(len(l1)):
+
+        if i == 0:
+            k = random.randrange(0, len(l1))
+        else:
+            while(In(str(k), lost)):
+                k = random.randrange(0, len(l1))
+        lost += str(k)
+        
+        l2.append(l1[k])
+    
+    return f'Lista original:    {l1} \nLista embaralhada: {l2}'
+
+print(SemPy())
